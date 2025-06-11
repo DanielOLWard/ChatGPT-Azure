cab0e3471960eb968884187f88006754eb9f8908
Daniel Lima <daniel2109lima@gmail.com>
2025-06-10 21:49:34 -0300

    JavaScript feito

---
+++ script.js
@@ -0,0 +1,71 @@
+async function sendMessage() {
+    const chatBox = document.getElementById("chatBox");
+
+    const userInput = document.getElementById("userInput");
+
+    const userMessage = userInput.value;
+
+    if(!userMessage) return;
+
+    const userDiv = document.createElement("div");
+    userDiv.className = "user-menssage message"
+    userDiv.textContent = userMessage;
+    chatBox.appendChild(userDiv);
+
+    userInput.value = "";
+
+    chatBox.scrollTop = chatBox.scrollHeight;
+
+    const endpoint = "";
+    const apiKey = "";
+    const deploymentId = "";
+    const apiVersion = "";
+
+    const url = `${endpoint}/openai/deployments/${deploymentId}/chat/completions?api-version=${apiVersion}`;
+
+    const data ={
+        messages: [{role: "user", content: userMessage}],
+        max_tokens:101,
+        temperature: 0.2, 
+    };
+
+    const headers = {
+        "Content-Type": "aplication/json",
+        "api-key": apiKey,
+    };
+
+    try {
+        const response = await fetch(url,{
+            method: "Post",
+            headers: headers,
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            const result = await response.JSON()
+            const botMessage = result.choices[0].message.content;
+
+            const botDiv = document.createElement("div");
+            botDiv.className = "bot-message message";
+            botDiv.textContent = botMessage;
+            chatBox.appendChild(botDiv);
+
+            chatBox.scrollTop = chatBox.scrollHeight;
+
+        } else {
+            console.error("Erro na requisição", response.status, response.statusText);
+
+            const botDiv = document.createElement("div");
+            botDiv.className = "bot-message message";
+            botDiv.textContent = "Erro ao se comunicar com o servico";
+            chatBox.appendChild(botDiv);
+        }
+
+    } catch (error) {
+        console.error("Error", error);
+
+        const botDiv = document.createElement("div");
+        botDiv.textContent = "Erro ao se comunicar com o servico";
+        chatBox.appendChild(botDiv);
+    }
+}
